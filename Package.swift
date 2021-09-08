e1a945ef5f691a3d7dbac3d50209cc93c1808b2b
Constantine Nikolsky <myheavybox@gmail.com>
2021-09-08 16:00:37 +0300

    Remove platforms restriction.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SLogFileBackend",
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
     products: [
         .library(
             name: "SLogFileBackend",
